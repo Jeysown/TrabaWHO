f4681715e849ecf0915ace9c6545342a2b0ea84a
Jeysown <jeysownlopez@gmail.com>
2016-12-05 13:53:05 +0800

    changes

---
+++ try.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+      <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="assets/fonts/css/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body id="page-top">
+    <!-- Navigation -->
+  <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header page-scroll">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+              </button>
+              <a class="navbar-brand page-scroll" href="#page-top">TrabaWHO</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                  <li class="hidden">
+                      <a href="#page-top"></a>
+                  </li>
+                  <li>
+                      <a class="page-scroll" href="#result">Result</a>
+                  </li>
+                  <li>
+                      <a class="page-scroll" href="#portfolio">Portfolio</a>
+                  </li>
+                  <li>
+                      <a class="page-scroll" href="#about">About</a>
+                  </li>
+                  <li>
+                      <a class="page-scroll" href="#team">Team</a>
+                  </li>
+                  <li>
+                      <a class="page-scroll" href="#contact">Contact</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+  <div class="" style="background-color:black;height:700px;">
+
+  </div>
+  <section id="result">
+    <div class="container marketing" style="margin-top:50px;">
+      <div class="row">
+    <div class="col-lg-12 text-center">
+        <h2 class="section-heading">Possible Result of the Test</h2>
+        <h3 class="section-subheading text-muted">Which Career Pathway is right for you?</h3>
+    </div>
+</div>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="col-lg-6">
+          <center><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="  width="140" height="140">
+          <h2 style="margin-bottom:0px;margin-top:20px;font-weight:bold;font-size:35px;">Realistic</h2>
+          <p>These people are often good at mechanical or athletic jobs. </p></center>
+          <div style="text-align:left;">
+          <h2>Course</h2>
+          <p>Agriculture and Fisheries, Marine, Engineering, Architectural, Hospitality Management, Medical, Computer Studies</p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="text-align:left;">
+          <h2 style="margin-top: 0px;">Work</h2>
+          <ul>
+            <li>Mechanical, manual, technical, athletic, agricultural</li>
+            <li>Tangible - using hands and manipulation</li>
+            <li>Concrete problem solving</li>
+            <li>Proactive predictable</li>
+          </ul>
+          <h2>Possible Occupation</h2>
+          <ul>
+            <li>Farmer, Fire fighter</li>
+            <li>Engineering</li>
+            <li>Carpenter, Electrician</li>
+            <li>Web Designer</li>
+            <li>Sculpture</li>
+            <li>Veterinarian</li>
+            <li>Physician</li>
+          </ul>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="col-lg-6">
+          <center><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="  width="140" height="140">
+          <h2 style="margin-bottom:0px;margin-top:20px;font-weight:bold;font-size:35px;">Investigative</h2>
+          <p>These people like to watch, learn, analyze and solve problems.  </p></center>
+          <div style="text-align:left;">
+          <h2>Course</h2>
+          <p>Course related to Science, and Mathematics, Economics, Finance, Accountancy, Education, Psychology,and Medical</p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="text-align:left;">
+          <h2 style="margin-top: 0px;">Work</h2>
+          <ul>
+            <li>Scientific and Mechanical</li>
+            <li>Intellectual, abstract,and critical thinking</li>
+            <li>Observing, investigating,and theorizing</li>
+            <li>Logical, precise methodical procedures in problem solving</li>
+          </ul>
+          <h2>Possible Occupation</h2>
+          <ul>
+            <li>Chemist, Biologist, Physicist</li>
+            <li>Actuarial scientist</li>
+            <li>Statistician</li>
+            <li>Mathematician</li>
+            <li>Dentist</li>
+            <li>Physician</li>
+          </ul>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="col-lg-6">
+          <center><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="  width="140" height="140">
+          <h2 style="margin-bottom:0px;margin-top:20px;font-weight:bold;font-size:35px;">Artistic</h2>
+          <p>These people like to work in unstructured situations where they can use their creativity.</p></center>
+          <div style="text-align:left;">
+          <h2>Course</h2>
+          <p>Communication Arts, Course Related to Music, Theatre Arts, Dancing, and Fine Arts</p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="text-align:left;">
+          <h2 style="margin-top: 0px;">Work</h2>
+          <ul>
+            <li>Creative skills in unstructured environment</li>
+            <li>Imaginative, innovative, creative, original</li>
+            <li>Expressive, abstract, aesthetic</li>
+            <li>Subjective, impulsive, introspective</li>
+            <li>Feminine and sensitive</li>
+          </ul>
+          <h2>Possible Occupation</h2>
+          <ul>
+            <li>Dancer, book editor</li>
+            <li>Art teacher</li>
+            <li>Fashion designer</li>
+            <li>Graphic designer</li>
+            <li>Actor</li>
+            <li>Disk Jockey</li>
+            <li>Composer</li>
+          </ul>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="col-lg-6">
+          <center><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="  width="140" height="140">
+          <h2 style="margin-bottom:0px;margin-top:20px;font-weight:bold;font-size:35px;">Social</h2>
+          <p>These people like to work with other people, rather than things. </p></center>
+          <div style="text-align:left;">
+          <h2>Course</h2>
+          <p>Psychology, Social Work, Nursing, Physical Therapy, Occupational Therapy, Education,and Library Science</p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="text-align:left;">
+          <h2 style="margin-top: 0px;">Work</h2>
+          <ul>
+            <li>Social, educational, and therapeutic skills</li>
+            <li>Cooperative, understanding, friendly</li>
+            <li>Value interpersonal relationships</li>
+            <li>Concerned with problems and growth of people</li>
+            <li>Emphasize idealism, generosity, ethical, and responsible</li>
+          </ul>
+          <h2>Possible Occupation</h2>
+          <ul>
+            <li>Conselor, social worker, nurse</li>
+            <li>Physical therapist</li>
+            <li>Occupational therapist</li>
+            <li>Teacher</li>
+            <li>Librarian</li>
+            <li>Athletic Trainer</li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container marketing">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="col-lg-6">
+          <center><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="  width="140" height="140">
+          <h2 style="margin-bottom:0px;margin-top:20px;font-weight:bold;font-size:35px;">Enterprising</h2>
+          <p>These people like to work with others and enjoy persuading and and performing.  </p></center>
+          <div style="text-align:left;">
+          <h2>Course</h2>
+          <p>Business Management, Law and Public Administration, Banking and Finance, Hospitality Management, Mass Communication, Psychology and Human Resource</p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="text-align:left;">
+          <h2 style="margin-top: 0px;">Work</h2>
+          <ul>
+            <li>Persuasive, manipulative, leadership skills</li>
+            <li>World of challenges to be overcome</li>
+            <li>Effective Speaker</li>
+            <li>Finance and economic issues important</li>
+            <li>Persuasion and selling essential tasks</li>
+          </ul>
+          <h2>Possible Occupation</h2>
+          <ul>
+            <li>Sales person, travel agent, account executive</li>
+            <li>Insurance underwriter</li>
+            <li>Judge</li>
+            <li>Lawyer</li>
+            <li>Bank president</li>
+            <li>Hotel manager</li>
+            <li>School head</li>
+          </ul>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="col-lg-6">
+          <center><img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="  width="140" height="140">
+          <h2 style="margin-bottom:0px;margin-top:20px;font-weight:bold;font-size:35px;">Conventional</h2>
+          <p>These people are very detail oriented,organized and like to work with data.</p></center>
+          <div style="text-align:left;">
+          <h2>Course</h2>
+          <p>Business Courses, Mathematics, Library Science,and Computer Studies</p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="text-align:left;">
+          <h2 style="margin-top: 0px;">Work</h2>
+          <ul>
+            <li>Systematic, Organization,and manipulation of data</li>
+            <li>World of facts that is practical and organized</li>
+            <li>Encourage and reward precise management of data and details</li>
+            <li>Clerical and organizational skills, dependability and ability to follow instructions</li>
+          </ul>
+          <h2>Possible Occupation</h2>
+          <ul>
+            <li>Clerk, secretary</li>
+            <li>Bank teller, mall carrier</li>
+            <li>Typist, bookkeeper, librarian</li>
+            <li>Computer Programmer</li>
+            <li>Technical writer</li>
+          </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  </body>
+</html>
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+
+      <script src="assets/js/header.js"></script>
+      <script src="assets/js/ease.js"></script>
